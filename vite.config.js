d5417f21a8304aa37231e50839dc2e8f008df628
Lane <lanewhitten14@gmail.com>
2025-03-29 15:41:24 -0500

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: '/2-divine-mercy-festival/', // <-- CHANGE THIS
+});
